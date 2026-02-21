d70afd91b2b785be7fb89cfa81dc2b2d515649c9
Octavi Fornés <octavi@fornes.cat>
2026-02-21 17:06:34 +0100

    Esquelet inicial del projecte

---
+++ src/main/java/cat/ofornes/idpkeycloak/IdpKeycloakApplication.java
@@ -0,0 +1,13 @@
+package cat.ofornes.idpkeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdpKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdpKeycloakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/ofornes/idpkeycloak/IdpKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.ofornes.idpkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdpKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
